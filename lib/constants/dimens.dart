384a805de12f4cfbaf41491b048fceee8cb48261
Pandu dwi Putra <pandu@pandudpn.id>
2021-09-25 11:47:28 +0700

    create constant and add googlefonts library

---
+++ lib/constants/dimens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dimens {
+  Dimens._();
+
+  static const double defaultPadding = 20.0;
+  static const double defaultMargin = 20.0;
+  static const double defaultHeight = 20.0;
+  static const double defaultRadius = 10.0;
+}
